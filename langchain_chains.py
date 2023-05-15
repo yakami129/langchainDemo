cf93673f9fe5224741583fe385e4f2f734d7dd23
yakami129 <770748371@qq.com>
2023-05-15 10:06:36 +0800

    init：langchain的学习demo

---
+++ langchain_chains.py
@@ -0,0 +1,9 @@
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+
+llm = OpenAI(temperature=0.9)
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template="What is a good name for a company that makes {product}?"
+)
+
---
+++ langchain_openai.py
@@ -0,0 +1,12 @@
+import os
+import openai
+
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+
+load_dotenv()  # 读取 .env 文件
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(temperature=0.9)
+text = "你是谁？"
+print(llm(text))
